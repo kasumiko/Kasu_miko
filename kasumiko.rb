ef9fb1dd37eff0abe1230713555a4e72f67fb916
kasumiko <ponywoodcommon@gmail.com>
2014-07-26 21:34:12 +0900

    namechanger
    up

---
+++ kasumiko.rb
@@ -0,0 +1,70 @@
+# encoding: utf-8
+require 'twitter'
+require 'tweetstream'
+require './userconf.rb'
+
+Streamclient = TweetStream::Client.new
+Streamclient.userstream do |status|
+
+ username = status.user.screen_name
+ contents = status.text
+ id = status.id
+
+unless contents =~ /RT/
+#namechange
+if contents =~ /@Kasu_miko update_name/
+name = contents.gsub(/@Kasu_miko update_name|\s|　/,"")
+  if name.length > 20 then
+   name = name[0,20]
+   @client.update_profile(name: "#{name}")
+   @client.update("#{name}に改名しました")
+  elsif name.length == 1 then
+   @client.update("#{"@" + username} 短すぎない？", :in_reply_to_status_id => id)
+  else
+   @client.update_profile(name: "#{name}")
+   @client.update("#{name}に改名しました")
+  end
+  @client.favorite(status)
+ end
+
+if contents =~ /@Kasu_miko update_profile/
+  description = contents.gsub(/@Kasu_miko update_profile/,"")
+  if description.bytesize > 160 then
+   description = description[0,80]
+   @client.update_profile(description: "#{description}")
+   @client.update("プロフィールを更新しました。"+"\u00A0"*rand(5))
+  else
+   @client.update_profile(description: "#{description}")
+   @client.update("プロフィールを更新しました。")
+  end
+  @client.favorite(status)
+end
+
+if contents =~ /@Kasu_miko update_location/
+  location = contents.gsub(/@Kasu_miko update_location/,"")
+  if location.length > 30 then
+   location = name[0,30]
+   @client.update_profile(locaton: "#{location}")
+   @client.update("今は#{location}にいます")
+  else
+   @client.update_profile(location: "#{location}")
+   @client.update("今は#{location}にいます")
+  end
+  @client.favorite(status)
+end
+=begin
+if contents =~ /@Kasu_miko update_icon/
+  location = contents.gsub(/@Kasu_miko update_location/,"")
+  if location.length > 30 then
+   location = name[0,30]
+   @client.update_profile(locaton: "#{location}")
+   @client.update("今は#{location}にいます")
+  else
+   @client.update_profile(location: "#{location}")
+   @client.update("今は#{location}にいます")
+  end
+  @client.favorite(status)
+end
+=end
+end
+end
